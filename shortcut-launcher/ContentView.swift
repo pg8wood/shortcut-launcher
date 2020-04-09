8c9616bb6b0271f132a5dbefce2a038c2588cd4e
Patrick Gatewood <pg8wood@vt.edu>
2020-04-09 14:03:20 -0400

    Initial Commit

---
+++ shortcut-launcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  shortcut-launcher
+//
+//  Created by Patrick Gatewood on 4/9/20.
+//  Copyright © 2020 Patrick Gatewood. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
